11577c6bca306bdf6eeda79784a7863d07047ece
Levie-PC <svpv.hotro@gmail.com>
2025-01-16 10:05:22 +0700

    Add date and time selection for booking in Checkout; implement fetching available times and update order details

--- src/pages/Checkout/Checkout.js
+++ src/pages/Checkout/Checkout.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Container, Grid, Typography, Card, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Select, MenuItem } from "@mui/material";
+import { Container, Grid, Typography, Card, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Select, MenuItem, TextField } from "@mui/material";
 import MKBox from "components/MKBox";
 import MKButton from "components/MKButton";
 import { useBooking } from "contexts/BookingContext";
@@ -11,13 +11,16 @@ import { get, post } from 'services/apiService';
 import { useAuth } from "contexts/AuthContext";
 
 function Checkout() {
-    const { bookingData, clearBooking } = useBooking();
+    const { bookingData, resetBooking } = useBooking();
     const { user } = useAuth();
     const navigate = useNavigate();
     const [pets, setPets] = useState([]);
     const [loading, setLoading] = useState(true);
     const [paymentMethod, setPaymentMethod] = useState("COD");
     const [ip, setIp] = useState("");
+    const [availableTimes, setAvailableTimes] = useState([]); // Add state for available times
+    const [selectedDate, setSelectedDate] = useState(new Date()); // Set default date to current date
+    const [selectedTime, setSelectedTime] = useState(""); // Add state for selected time
 
     useEffect(() => {
         const fetchPets = async () => {
@@ -46,6 +49,22 @@ function Checkout() {
             });
     }, []);
 
+    useEffect(() => {
+        if (selectedDate) {
+            const fetchAvailableTimes = async () => {
+                try {
+                    const response = await get(`/api/public/spa/get-available-times?date=${selectedDate.toISOString().split('T')[0]}`);
+                    if (response.data.status === 200) {
+                        setAvailableTimes(response.data.data);
+                    }
+                } catch (error) {
+                    console.error("Error fetching available times:", error);
+                }
+            };
+            fetchAvailableTimes();
+        }
+    }, [selectedDate]);
+
     const handleConfirmPayment = async () => {
         try {
             const orderDetails = {
@@ -53,7 +72,7 @@ function Checkout() {
                 paymentMethod: paymentMethod.toUpperCase(),
                 goodsType: "SPA",
                 cart: bookingData.selectedServices.map((service) => ({
-                    id: service.id,
+                    id: service.serviceId,
                     quantity: 1,
                 })),
                 subtotal: bookingData.selectedServices.reduce((acc, service) => acc + service.price, 0),
@@ -62,6 +81,9 @@ function Checkout() {
                 name: user.name,
                 phone: user.phone,
                 address: "",
+                startTime: selectedTime, // Add selected time to order details
+                date: selectedDate.toISOString().split('T')[0], // Add selected date to order details
+                petId: bookingData.petDetails.id,
             };
 
             const formData = new FormData();
@@ -69,9 +91,11 @@ function Checkout() {
 
             const response = await post("/api/user-order/createOrder", formData, true);
             if (response.data.status === 200) {
+                resetBooking();
+                window.history.replaceState({}, document.title);
                 console.log("Order confirmed:", response.data);
                 if (paymentMethod === "COD") {
-                    clearBooking();
+                    resetBooking();
                     window.history.replaceState({}, document.title);
                     Swal.fire({
                         icon: 'success',
@@ -86,8 +110,12 @@ function Checkout() {
                 formData2.append("ip", ip);
                 const response2 = await post("/api/payment/create-payment", formData2, true);
                 if (response2.data.status === 200) {
+                    resetBooking();
+                    window.history.replaceState({}, document.title);
                     console.log("Payment gate url:", response2.data.data);
                     window.location.href = response2.data.data;
+                } else {
+                    console.error("Error creating payment:", response2.data.message);
                 }
             } else {
                 console.error("Error confirming order:", response.data.message);
@@ -119,6 +147,17 @@ function Checkout() {
 
         }
     };
+
+    const isToday = (date) => {
+        const today = new Date();
+        return date.getDate() === today.getDate() &&
+            date.getMonth() === today.getMonth() &&
+            date.getFullYear() === today.getFullYear();
+    };
+
+    const minDate = new Date();
+    const maxDate = new Date();
+    maxDate.setMonth(maxDate.getMonth() + 2);
 
     return (
         <>
@@ -212,6 +251,51 @@ function Checkout() {
                                     </MKBox>
                                 </Grid>
                                 <Grid item xs={12} md={4}>
+                                    <MKBox mb={3}>
+                                        <Typography variant="h6">Select Date</Typography>
+                                        <TextField
+                                            label="Date"
+                                            type="date"
+                                            value={selectedDate ? selectedDate.toISOString().split('T')[0] : ""}
+                                            onChange={(e) => setSelectedDate(new Date(e.target.value))}
+                                            InputLabelProps={{ shrink: true }}
+                                            inputProps={{
+                                                min: minDate.toISOString().split('T')[0],
+                                                max: maxDate.toISOString().split('T')[0],
+                                            }}
+                                            fullWidth
+                                            sx={{ mb: 2 }}
+                                        />
+                                    </MKBox>
+                                    <MKBox mb={3}>
+                                        <Typography variant="h6">Select Time</Typography>
+                                        <Select
+                                            value={selectedTime}
+                                            onChange={(e) => setSelectedTime(e.target.value)}
+                                            fullWidth
+                                            displayEmpty
+                                        >
+                                            <MenuItem value="" disabled>
+                                                Select a time
+                                            </MenuItem>
+                                            {availableTimes.map((time) => {
+                                                const currentTime = new Date(); // Thời gian hiện tại
+                                                const selectedDateTime = new Date(`${selectedDate.toISOString().split('T')[0]}T${time.time}:00`);
+
+                                                // Kiểm tra nếu ngày là hôm nay và giờ nhỏ hơn thời gian hiện tại
+                                                const isDisabled =
+                                                    !time.isAvailable ||
+                                                    (isToday(selectedDate) && selectedDateTime < currentTime);
+
+                                                return (
+                                                    <MenuItem key={time.time} value={time.time} disabled={isDisabled}>
+                                                        {time.time} {time.isAvailable ? "" : "(Unavailable)"}
+                                                    </MenuItem>
+                                                );
+                                            })}
+                                        </Select>
+                                    </MKBox>
+
                                     <MKBox mb={3}>
                                         <Typography variant="h6">Payment Method</Typography>
                                         <Select
